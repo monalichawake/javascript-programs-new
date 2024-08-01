4095025a64a828351a01430c2809bc6836149f91
monalichawake <monali.chawake1112@gmail.com>
2024-08-01 08:57:37 +0530

    javascript variables
    javascript variables

---
+++ 01-variable-assignment.js
@@ -0,0 +1,5 @@
+console.log("My City Name: Pune");
+console.log("My best friend Name: Rutuja");
+console.log("My country Name: India");
+console.log("My school Name: Sushil Himmatsingka Wardha");
+console.log("My college Name: S.D college of Engineering,Selukate Wardha");
