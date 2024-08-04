8a897a3ce5f8ad74e03f81a85b34c124a6180a02
monalichawake <monali.chawake1112@gmail.com>
2024-08-04 22:25:49 +0530

    javascript stringHandon.js
    javascript stringHandon.js

--- 03-stringBasicAssig01.js
+++ 03-stringBasicAssig01.js
@@ -4,9 +4,6 @@ console.log("My dream company is: ", greet);
 var greet = "Rope Mulkhab, Dancing, Reading";
 console.log("My hobbies are: ", greet);
 
-var firstNumber = "10";
-var secontName = " 20";
-var thirdName = " 20";
-var concatString = 10+20+20;
-console.log("Total number of sum:",concatString );
+var greetLength = greet.length;
+console.log("Total number of characters is: ",greetLength);
 
---
+++ 03-stringHandOn.js
@@ -0,0 +1,62 @@
+console.log("==========Assignment1=============");
+var sentence = "  Hey you are doing good, keep it up   ";
+console.log("Given string is: ", sentence);
+
+console.log("===========2===========");
+var sentenceLength = sentence.length;
+console.log("Total number of characters is: ",sentenceLength);
+
+console.log("==========3==========");
+var sentence = "   Hey you are doing good, keep it up    ";
+var result = sentence.trim();
+console.log(`Given sentence is: ${result} and it's length is: ${result.length}`);
+
+console.log("==========4==========");
+var sentence = "   Hey you are doing good, keep it up    ";
+var sentenceAfterTrim = sentence.trim();
+var sentenceAfterTrimLength= sentenceAfterTrim.length;
+console.log("After string trim length is: ", sentenceAfterTrimLength);
+console.log("Total number of extra space removed: ",sentenceLength- sentenceAfterTrimLength);
+
+console.log("===========5=========");
+var myLove = "Hey you are doing good, keep it up";
+var result = myLove.split(" ");
+console.log("Total number of words: ", result.length);
+
+console.log("==========6==========");
+var result = sentence.indexOf("good");
+console.log("Index of character good is: ", result);
+
+console.log("========7=========");
+var result = sentence.indexOf("22");
+console.log("Substring staring from index 22,keep it up ", result);
+
+console.log("==========8==========");
+var result = sentence.includes("Hey");
+var result = sentence.includes("Hey")
+console.log("Starting start with Hey?:", result);
+
+console.log("=========9========");
+var result = sentence.includes("up");
+var result = sentence.includes("up")
+console.log("Starting start end with up?:",result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
